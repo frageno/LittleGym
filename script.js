05766b27b8685e9bb33210a3ba22e85b3059e04c
unknown <szymon.piatkowski27@gmail.com>
2020-06-01 22:37:53 +0200

    New version with scroll effect
    I added scroll method using jQuery which allows nicely transform
     our text or image.

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ $(document).ready(()=>{
     $('.nav-button').click(()=>{
         $('.nav-button').toggleClass('c-hamburger__change');
     });
+
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position>=180){
+            $('.t-mission__img').addClass('t-mission__effect-right');
+            $('.t-mission__second-title').addClass('t-mission__effect-left');
+        }
+        else{
+            $('.t-mission__img').removeClass('t-mission__effect-right');
+            $('.t-mission__second-title').removeClass('t-mission__effect-left');
+
+
+        }
+    });
 })
